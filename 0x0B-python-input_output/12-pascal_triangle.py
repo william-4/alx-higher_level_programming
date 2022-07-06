680e2c862fff998da5c036bec9a867eab97f99a6
william-4 <mwangiiwilliam@gmail.com>
2022-07-06 06:00:29 +0300

    task 12 first commit

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Defines a function pascal_triangle
+"""
+
+
+def pascal_trianble(n):
+    """
+    Returns a list of lists of integers representing Pascal's triangle of n
+    """
+    my_list = []
+    if n <= 0:
+        return (my_list)
+    return True
