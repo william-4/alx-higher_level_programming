b192538c14722b1df11c5e41134ead51e997b15d
william-4 <mwangiiwilliam@gmail.com>
2022-07-12 05:08:04 +0300

    new commit

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+This module defines a class called Base
+"""
+
+
+class Base:
+    """
+    The base class for all other classes in this project
+    """
+
+    ''' private class attribute for number of objects'''
+    __nb_objects = 0
+    def __init__(self, id=None):
+        """
+        Initialize a Base instance
+
+        Args:
+        id (int): id of a new Base instance
+        """
+        if id is None:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+        else:
+            self.id = id
