0a036cee70eefcab74d969cabf4cd22890b90054
Liam <mwangiiwilliam@gmail.com>
2022-09-10 06:48:19 +0300

    add grant in 1 and fix syntax in 2

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,2 +1,5 @@
 -- A script that creates a user with a password
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL
+	  ON *.*
+	  TO 'user_0d_1'@'localhost';
