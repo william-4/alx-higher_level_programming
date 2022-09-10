37340383cdb802d68e36a686ecae2c61e502cff2
Liam <mwangiiwilliam@gmail.com>
2022-09-10 07:53:40 +0300

    hot fix task 7

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -3,11 +3,9 @@
 -- state_id can't be null, is a foreign key that references id of states table
 -- name can't be null
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
-CREATE TABLE IF NOT EXISTS `cities` (
-	   PRIMARY KEY (`id`),
-	   `id` INT NOT NULL AUTO_INCREMENT,
-	   `state_id` INT NOT NULL,
-	   `name` VARCHAR(256) NOT NULL,
-	   FOREIGN KEY(`state_id`)
-	   REFERENCES `hbtn_0d_usa`.`states`(`id`)
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+	   id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	   state_id INT NOT NULL,
+	   name VARCHAR(256) NOT NULL,
+	   FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id)
 );
