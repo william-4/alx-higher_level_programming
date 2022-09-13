ba21876a257689fa82d88f7dae74353a9b237d69
Liam <mwangiiwilliam@gmail.com>
2022-09-13 20:40:40 +0300

    finally realized my files were not executables

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,10 +5,9 @@ if __name__ == '__main__':
     import MySQLdb
     import sys
 
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                           passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT id, name FROM states ORDER BY id;")
+    cur.execute("SELECT * FROM states ORDER BY id;")
     rows = cur.fetchall()
     for row in rows:
         print(row)
