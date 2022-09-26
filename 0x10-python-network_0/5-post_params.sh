662b5f767d6595f49fcc8cf449b99f3c51e62c0b
william-4 <mwangiiwilliam@gmail.com>
2022-09-26 04:55:22 +0300

    wc -l 3

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# curl sends a POST request and display response body
-# send data with the request using -d option
+# curl sends a POST request with data and display response body
 curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
